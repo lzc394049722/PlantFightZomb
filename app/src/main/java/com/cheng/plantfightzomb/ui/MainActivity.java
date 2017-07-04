bca0ad2c679b8648bf0ec7826b7ce01bdf2ca5ac
lzc394049722 <394049722@qq.com>
2017-07-04 17:31:43 +0800

    测试一下

--- app/src/main/java/com/cheng/plantfightzomb/ui/MainActivity.java
+++ app/src/main/java/com/cheng/plantfightzomb/ui/MainActivity.java
@@ -115,8 +115,6 @@ public class MainActivity extends AppCompatActivity implements BasePlant.MyCallB
                                             return;
                                         }
                                     }
-
-
                                 }
                             }
                         }
